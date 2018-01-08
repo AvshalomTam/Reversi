39c2b8bfc2926b246bc520a111ce781c87af994a
AvshalomTam <avshalom.tam@gmail.com>
2018-01-09 00:05:52 +0200

    continued working on abstract Player and class HumanPlayer - not finished with HumanPlayer

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,2 +1,100 @@
+/**
+ * Representing a human player.
+ */
 public class HumanPlayer extends Player {
+    //members
+    private Board board_;
+    private GameLogic judge_;
+    private Display gameflow_;
+    private Listener listener_;
+    /**
+     * Constructor.
+     *
+     * @param numplayer first_player or second_player
+     * @param board     Board object
+     * @param judge     Gamelogic object
+     * @param gameflow  Display object
+     */
+    public HumanPlayer(cell numplayer, Board board, GameLogic judge, Display gameflow, Listener listener) {
+        super(numplayer);
+        this.board_ = board;
+        this.judge_ = judge;
+        this.gameflow_ = gameflow;
+        this.listener_ = listener;
+    }
+
+    /**
+     * Gets coordinates in board where the player wants to put his tile
+     *
+     * @return coordinates
+     */
+    public Coordinates getMove() {
+        Coordinates c = new Coordinates(-1, -1);
+        try {
+            if (!this.judge_.hasOptions(this.board_, this.getId())) {
+                this.listener_.hadMove(false);
+                this.listener_.setPreMove(c);
+                this.listener_.setPreName(this.getName());
+                this.hasMove(false);
+
+                return c;
+            }
+        } catch (e) {
+            throw e;
+        }
+
+        Coordinates input;
+        this.hasMove(true);
+
+        while (true) {
+            try {
+                String s = this.gameflow_.input();
+                input = Coordinates(s).move(Coordinates(-1, -1));
+                //checking if player's choice is one of the options
+                if (this.judge_.isValidChoice(this.board_, input, this.getId())) {
+                    break;
+                } else {
+                    throw "That's none of your choices!";
+                }
+            } catch (error) {
+                this.gameflow_.printInputError(error);
+                this.gameflow_.printMoveRequest();
+                continue;
+            }
+        }
+        try {
+            this.listener_.hadMove(true);
+            this.listener_.setPreMove(input);
+            this.listener_.setPreName(this.getName());
+            this.hasMove(true);
+            return input;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Prints an appropiate message.
+     */
+    public void message() {
+        //prints previous player move
+        if (this.listener_.preHadMove()){
+            this.gameflow_.printPreviousMove(this.listener_.getName(), (this.
+            listener_.getPreMove().move(Coordinates(1, 1)).toString()));
+        }
+
+        //prints whose turn it is
+        this.gameflow_.printTurn(this.getName());
+
+        if (!this.judge_.hasOptions(this.board_, this.getId())){
+            this.gameflow_.printNoMove();
+            return;
+        }
+        list<Coordinates> options = this.judge_.getOptions(this.board_, this.getId());
+        //printing the player's options
+        this.gameflow_.printOptions(options);
+
+        this.gameflow_.printMoveRequest();
+
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,50 @@
+/**
+ * Representing a player in the game.
+ */
 public abstract class Player {
+
+    private Boolean had_move_;
+    private cell player_id_;
+    private String name_;
+    /**
+     * Constructor.
+     */
+    public Player(cell num_player) {
+        this.had_move_ = false;
+        this.player_id_ = num_player;
+    }
+    /**
+     * Sets the name of the player for this game.
+     * @param name string
+     */
+    public void setName(String name) {
+        this.name_ = name;
+    }
+    /**
+     * @return name of the player
+     */
+    public String getName() {
+        return this.name_;
+    }
+    /**
+     * Checks if the player had a move.
+     * @return true if player had a move, false otherwise
+     */
+    public void hasMove(Boolean played) {
+        this.had_move_ = played;
+    }
+    /**
+     * Checks if the player had a move.
+     * @return true if player had a move, false otherwise
+     */
+    public Boolean played() {
+        return this.had_move_;
+    }
+    /**
+     * @return number of the player, 1 or 2
+     */
+    public cell getId() {
+        return this.player_id_;
+    }
+    abstract void message();
 }
