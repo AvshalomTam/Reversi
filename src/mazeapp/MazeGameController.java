33f788dba20fb751d81293902d66e4b15fa11110
SimSteve <simgutfreund@gmail.com>
2018-01-09 18:18:51 +0200

    copied the MazeBoard game

---
+++ src/mazeapp/MazeGameController.java
@@ -0,0 +1,25 @@
+package mazeapp;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MazeGameController implements Initializable {
+    @FXML
+    private HBox root;
+    private int[][] maze = {
+            {0,1,0,1,0,0,0,1,0,0,0},
+            {0,1,0,1,1,1,0,1,0,1,0}};
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        MazeBoard mazeBoard = new MazeBoard(maze);
+        mazeBoard.setPrefWidth(400);
+        mazeBoard.setPrefHeight(400);
+        root.getChildren().add(0, mazeBoard);
+        mazeBoard.draw();
+    }
+}
