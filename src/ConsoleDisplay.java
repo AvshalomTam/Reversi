a330ba676c0ce420187ba87a79711daf2ed88c8c
AvshalomTam <avshalom.tam@gmail.com>
2018-01-09 08:51:30 +0200

    continued working on abstract Player and class HumanPlayer - not finished with HumanPlayer

---
+++ src/ConsoleDisplay.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class ConsoleDisplay implements Display{
+
+
+    @Override
+    public void printCurrentBoard(Board board) {
+
+    }
+
+    @Override
+    public void printPreviousMove(String name, String coordinate) {
+
+    }
+
+    @Override
+    public void printTurn(String name) {
+
+    }
+
+    @Override
+    public void printNoMove() {
+
+    }
+
+    @Override
+    public void printOptions(ArrayList<Coordinates> options) {
+
+    }
+
+    @Override
+    public void printMoveRequest() {
+
+    }
+
+    @Override
+    public void printInputError(String error) {
+
+    }
+
+    @Override
+    public void printResult(String name) {
+
+    }
+
+    @Override
+    public String input() {
+        return null;
+    }
+
+    @Override
+    public void printGameName(String name) {
+
+    }
+
+    @Override
+    public String getGameName() {
+        return null;
+    }
+
+    @Override
+    public void presentPlayer(String name) {
+
+    }
+}
