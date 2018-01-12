1f2bb98ad4f97e0215aa9754d0c425ffd67fbb72
SimSteve <simgutfreund@gmail.com>
2018-01-12 14:34:43 +0200

    added two buttons to the game; quit and exit

--- src/GUI/InfoController.java
+++ src/GUI/InfoController.java
@@ -3,14 +3,17 @@ package GUI;
 import Game.GameStatus;
 import Game.cell;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class InfoController extends VBox {
     private FXMLLoader loader;
     public GameStatus status;
+    public Button btn;
 
     public InfoController(GameStatus status) {
         this.status = status;
@@ -36,5 +39,19 @@ public class InfoController extends VBox {
 
         lbl = new Label(this.status.getPl2Name() + " player score is: " + this.status.getScore(cell.second_player));
         this.getChildren().add(2, lbl);
+
+        this.btn = new Button("Quit Game");
+        this.btn.setOnAction(event -> {
+            Stage stage = (Stage) this.btn.getScene().getWindow();
+            stage.setTitle("Reversi");
+            stage.setScene(new SceneFactory().getMainMenuScene());
+        });
+        this.getChildren().add(3, this.btn);
+
+        this.btn = new Button("Exit");
+        this.btn.setOnAction(event -> {
+            System.exit(0);
+        });
+        this.getChildren().add(4, this.btn);
     }
 }
